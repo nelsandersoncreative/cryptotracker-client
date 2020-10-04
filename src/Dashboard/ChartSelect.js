c8cdd3faff2ed87519b11ff8383bbf51da5283de
Nels Anderson <nelsandersoncreative@gmail.com>
2020-10-04 09:54:43 -0700

    mobile css

--- src/Dashboard/ChartSelect.js
+++ src/Dashboard/ChartSelect.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 // styles for days/weeks/months pulldown menu within PriceChart component (data visualization)
 export default styled.select`
-  -webkit-appearance:none;
   background-color: #061a44;
   outline: 0;
   font-size: 30px;
@@ -13,7 +12,9 @@ export default styled.select`
   margin: 0;
   width: 100%
   height: 60px;
-  @media (max-width: 780px) {
+  @media (max-width: 500px) {
+    -webkit-appearance: none;
+    background: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2ZmZjt9LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHJlY3QgY2xhc3M9ImNscy0xIiB3aWR0aD0iNC45NSIgaGVpZ2h0PSIxMCIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIxLjQxIDQuNjcgMi40OCAzLjE4IDMuNTQgNC42NyAxLjQxIDQuNjciLz48cG9seWdvbiBjbGFzcz0iY2xzLTIiIHBvaW50cz0iMy41NCA1LjMzIDIuNDggNi44MiAxLjQxIDUuMzMgMy41NCA1LjMzIi8+PC9zdmc+) no-repeat 95% 50%;
     width: 90%;
     margin-top: 5px;
   }
