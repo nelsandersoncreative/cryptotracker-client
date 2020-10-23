9f13c979e6b71060147698f894f1fffcde62fbb1
Nels Anderson <nelsandersoncreative@gmail.com>
2020-10-22 20:16:21 -0700

    added sample credentials

--- src/Settings/WelcomeMessage.js
+++ src/Settings/WelcomeMessage.js
@@ -10,6 +10,13 @@ export default function({firstVisit}) {
         firstVisit && !hasAuth ? <div>
         <h1 className="welcome-head">Welcome to CryptoTracker</h1>
         <h3 className="welcome-desc">This app allows you to track chronological price data for your favorite cryptocurrencies!</h3>
+        <p className="welcome-steps">
+        <ul className="welcome-list">
+          <li>Sample account to view the onboarding experience</li>
+          <li>USERNAME: sampleuser@sampleuser.com</li>
+          <li>PASSWORD: foobar</li>
+        </ul>
+        </p>
         <p className="welcome-steps">1. Register for an account or Login.</p>
         <p className="welcome-steps">2. Select coins below or search for them in the search bar.</p>
         <p className="welcome-steps">3. Click the coins to add them to your favorites.</p>
